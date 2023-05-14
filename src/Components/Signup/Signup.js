960cb3e5a3f435c8fd5c8c06b22173e6f6293da0
Abin <abinth250@gmail.com>
2023-05-15 00:28:43 +0530

    state variables added for signup

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const [username,setUsername] = useState('')
+  const [email,setEmail] = useState('')
+  const [phone,setPhone] = useState('')
+  const [password,setPassword] = useState('')
+  // const [username,setUsername] = useState('')
+  // const [username,setUsername] = useState('')
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const user = {
+      username,
+      email,
+      phone,
+      password
+    }
+    console.log(user)
+  }
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
@@ -16,6 +33,8 @@ export default function Signup() {
             type="text"
             id="fname"
             name="name"
+            value={username}
+            onChange={(e)=>{setUsername(e.target.value)}}
             defaultValue="John"
           />
           <br />
@@ -26,6 +45,8 @@ export default function Signup() {
             type="email"
             id="fname"
             name="email"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
             defaultValue="John"
           />
           <br />
@@ -36,6 +57,8 @@ export default function Signup() {
             type="number"
             id="lname"
             name="phone"
+            value={phone}
+            onChange={(e)=>{setPhone(e.target.value)}}
             defaultValue="Doe"
           />
           <br />
@@ -46,6 +69,8 @@ export default function Signup() {
             type="password"
             id="lname"
             name="password"
+            value={password}
+             onChange={(e)=>{setPassword(e.target.value)}}
             defaultValue="Doe"
           />
           <br />
